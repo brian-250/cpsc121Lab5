c678d91f0f7afc63a66656129f040d614cf6af0c
Brian Milian <106508985+brian-250@users.noreply.github.com>
2023-02-28 07:47:48 +0000

    Wrote Lab 5 prob01 code and there is one make test error

--- prob01/main.cc
+++ prob01/main.cc
@@ -1,22 +1,33 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Brian Milian
+// CPSC 121L-01
+// 2023-02-21
+// brianmilian@csu.fullerton.edu
+// @brian-250
 //
 // Lab 5-1
 // If it is a pair programming lab please specify partner below.
 // Partner: @peteranteater
+//
+// This program prompts the user to enter the number of
+// elements they want in a vector and the program
+// allows them to choose three values they want to
+// put in the vector. The program then return the index
+// of the smallest value in the vector.
+//
 
 #include <iostream>
 #include <vector>
+
+#include "minimum.h"
 
 int main() {
   // First, we ask the user what size vector they want to create.
   int size = 0;
   std::cout << "How many elements? ";
   std::cin >> size;
+  if (size == 0) {
+    return -1;
+  }
 
   // Construct a vector of doubles with the given size.
   // This initializes all the elements in the vector to the
@@ -38,5 +49,8 @@ int main() {
   // Don't forget to #include "minimum.h" so you can
   // make a call to the IndexOfMinimumElement declared there.
   // ========================================================
-  std::cout << "The minimum value in your vector is at index ";
+
+  int small_index{IndexOfMinimumElement(numbers)};
+  std::cout << "The minimum value in your vector is at index " << small_index
+            << "\n";
 }
--- prob01/minimum.cc
+++ prob01/minimum.cc
@@ -1,13 +1,19 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Brian Milian
+// CPSC 121L-01
+// 2023-02-21
+// brianmilian@csu.fullerton.edu
+// @brian-250
 //
 // Lab 5-1
 // If it is a pair programming lab please specify partner below.
 // Partner: @peteranteater
+//
+// This program prompts the user to enter the number of
+// elements they want in a vector and the program
+// allows them to choose three values they want to
+// put in the vector. The program then return the index
+// of the smallest value in the vector.
+//
 
 #include "minimum.h"
 
@@ -17,5 +23,17 @@ int IndexOfMinimumElement(std::vector<double> &input) {
   // vector, and return it. If the input vector is empty,
   // return -1.
   // ========================================================
-  return -1;
+  int small_index{0};
+  double min{input.at(0)};
+
+  if (input.empty()) {
+    return -1;
+  }
+  for (int i = 0; i < input.size(); i++) {
+    if (input.at(i) < min) {
+      min = input.at(i);
+      small_index = i;
+    }
+  }
+  return small_index;
 }
